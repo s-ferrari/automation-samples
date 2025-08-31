e9e154a77a6a31f062ecdaffb391a08c18159a32
s-ferrari <sol_ferrari20@hotmail.com>
2025-08-31 19:15:41 -0300

    refactor price calculation

--- cypress/e2e/Price-Calculation.cy.js
+++ cypress/e2e/Price-Calculation.cy.js
@@ -1,5 +1,30 @@
-//const { xit } = require("mocha");
+let prices = ["99", "100", "101", "199", "200", "201"];
+let weights = ["4", "5", "6"];
+let cards = [true, false];
 
+
+it('All cases', () => {
+  cy.visit('https://exercises.test-design.org/price-calculation/')
+  for (let price of prices) {
+    cy.get('[data-harmony-id="Price"]').type(price);
+    for (let weight of weights) {
+      cy.get('[data-harmony-id="Weight"]').type(weight);
+      for (let card of cards) {
+        if (card === true) {
+          cy.get('[data-harmony-id="Card"]').check();
+        }
+        cy.get('[data-harmony-id="Add test"]').click();
+        cy.get('[data-harmony-id="Card"]').uncheck();
+      }
+      cy.get('[data-harmony-id="Weight"]').clear();
+    }
+    cy.get('[data-harmony-id="Price"]').clear();
+  }
+  cy.get('[data-harmony-id="Stop"]').click();
+})
+
+
+/*
 describe('Price Calculation', () => {
   it('Without card', () => {
     cy.visit('https://exercises.test-design.org/price-calculation/')
@@ -17,30 +42,4 @@ describe('Price Calculation', () => {
     cy.get('[data-harmony-id="Add test"]').click();
     cy.get('[data-harmony-id="Total price"]').should('have.text', '170');
   })
-
-  let prices = ["99", "100", "101", "199", "200", "201"];
-  let weights = ["4", "5", "6"];
-  let cards = [true, false];
-
-
-  it('All cases', () => {
-    cy.visit('https://exercises.test-design.org/price-calculation/')
-    for (let price of prices) {
-      cy.get('[data-harmony-id="Price"]').type(price);
-      for (let weight of weights) {
-        cy.get('[data-harmony-id="Weight"]').type(weight);
-        for (let card of cards) {
-          if (card === true) {
-            cy.get('[data-harmony-id="Card"]').check();
-          }
-          cy.get('[data-harmony-id="Add test"]').click();
-          cy.get('[data-harmony-id="Card"]').uncheck();
-        }
-        cy.get('[data-harmony-id="Weight"]').clear();
-      }
-        cy.get('[data-harmony-id="Price"]').clear();
-    }
-    cy.get('[data-harmony-id="Stop"]').click();
-    //cy.get('[data-harmony-id="Total price"]').should('have.text','170');
-  })
-})
+})*/
