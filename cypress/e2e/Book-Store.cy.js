aaaea0c96b3a6a32d31f23cb402150b93bb5f34d
s-ferrari <sol_ferrari20@hotmail.com>
2025-08-31 23:42:12 -0300

    refactor: rename variables and fix test loop logic in Book Store

---
+++ cypress/e2e/Book-Store.cy.js
@@ -0,0 +1,28 @@
+describe('Book Store', () => {
+
+  it('Book Store', () => {
+    cy.visit('https://exercises.test-design.org/online-book-store/')
+
+    let newPrices = [40, 49.99, 50, 29, 30, 30.01, 60];
+    let oldPrices = [59, 60.01, 61, 60, 0];
+    let vipCustomers = [true, false];
+
+    for (let newPrice of newPrices) {
+      cy.get('[data-harmony-id="New price"]').clear().type(newPrice);
+
+      for (let oldPrice of oldPrices) {
+        cy.get('[data-harmony-id="Second price"]').clear().type(oldPrice);
+
+        for (let vipCustomer of vipCustomers) {
+          if (vipCustomer) {
+            cy.get('[data-harmony-id="Card"]').check();
+          } else {
+            cy.get('[data-harmony-id="Card"]').uncheck();
+          }
+          cy.get('[data-harmony-id="Add test"]').click();
+        }
+      }
+    }
+    cy.get('[data-harmony-id="Stop"]').click();
+  })
+})
